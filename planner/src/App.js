976e2af241247a11e028bbcddc6f2165408d0f21
CourtneyHoppus <74206103+CourtneyHoppus@users.noreply.github.com>
2021-04-20 18:22:42 -0700

    lessons #4-10

--- planner/src/App.js
+++ planner/src/App.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Dashboard from './components/dashboard/Dashboard';
+import ProjectDetail from './components/projects/ProjectDetail';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
+import CreateProject from './components/projects/CreateProject';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <h1>Planner</h1>
-      </div>
-    </BrowserRouter>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={Dashboard} />
+            <Route path='/project/:id' component={ProjectDetail} />
+            <Route path='/signin' component={SignIn} />
+            <Route path='/signup' component={SignUp} />
+            <Route path='/create' component={CreateProject} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
---
+++ planner/src/components/projects/ProjectDetail.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const ProjectDetail = (props) => {
+  const id = props.match.params.id;
+  return (
+    <div className='container section project-details'>
+      <div className='card z-depth-0'>
+        <div className='card-content'>
+          <span className='card-title'>Project Title - {id}</span>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna
+            aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+            ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+        <div className='card-action grey lighten-4 grey-text'>
+          <div>Posted be me, Courtney</div>
+          <div>2021-04-20</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProjectDetail;
